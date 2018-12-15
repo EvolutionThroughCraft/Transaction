412598c664a9301b95b8c6098f794d26670e40f7
Dwin <dwin.e357@gmail.com>
2018-12-15 10:53:47 -0500

    git subrepo pull (merge) src/main/java/io/github/evolutionThroughCraft/common/service
    subrepo:
      subdir:   "src/main/java/io/github/evolutionThroughCraft/common/service"
      merged:   "17b27d3"
    upstream:
      origin:   "https://github.com/EvolutionThroughCraft/CommonServicePackage.git"
      branch:   "master"
      commit:   "be1f977"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "5d6aba9"

---
+++ src/main/java/io/github/evolutionThroughCraft/common/service/main/clients/TransactionClient.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.evolutionThroughCraft.common.service.main.clients;
+
+/**
+ *
+ * @author dwin
+ */
+public class TransactionClient {
+    
+    public Transaction post(Transaction transaction) {
+        return null;
+    }
+}
