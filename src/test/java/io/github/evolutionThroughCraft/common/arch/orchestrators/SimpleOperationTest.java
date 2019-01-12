2753a0f8cedc5bdb318ff59c8e9d26ecfc53cf0d
Dwin <dwin.e357@gmail.com>
2019-01-11 20:09:37 -0500

    git subrepo clone https://github.com/EvolutionThroughCraft/CommonArchitectureTest.git src/test/java/io/github/evolutionThroughCraft/common/arch/
    subrepo:
      subdir:   "src/test/java/io/github/evolutionThroughCraft/common/arch"
      merged:   "e5a17b3"
    upstream:
      origin:   "https://github.com/EvolutionThroughCraft/CommonArchitectureTest.git"
      branch:   "master"
      commit:   "e5a17b3"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "5d6aba9"

---
+++ src/test/java/io/github/evolutionThroughCraft/common/arch/orchestrators/SimpleOperationTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.evolutionThroughCraft.common.arch.orchestrators;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dwin
+ */
+public class SimpleOperationTest {
+    
+    
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
