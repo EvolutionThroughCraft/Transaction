84dcc4adcb7baba5da36582789d181d3c8d27efc
Dwin <dwin.e357@gmail.com>
2019-01-11 20:09:28 -0500

    git subrepo clone https://github.com/EvolutionThroughCraft/CommonServicePackageTest.git src/test/java/io/github/evolutionThroughCraft/common/service/
    subrepo:
      subdir:   "src/test/java/io/github/evolutionThroughCraft/common/service"
      merged:   "27ab92d"
    upstream:
      origin:   "https://github.com/EvolutionThroughCraft/CommonServicePackageTest.git"
      branch:   "master"
      commit:   "27ab92d"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "5d6aba9"

---
+++ src/test/java/io/github/evolutionThroughCraft/common/service/main/clients/AccountClientTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.evolutionThroughCraft.common.service.main.clients;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dwin
+ */
+public class AccountClientTest {
+    
+    
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
